9a9e41921842491b83db99ddb82968096f4971e2
Gorlomir <151527759+Gorlomir@users.noreply.github.com>
2025-01-27 14:18:00 +0500

    Update module_10_2.py
    добавил lock

--- module_10_2.py
+++ module_10_2.py
@@ -3,9 +3,10 @@ import time
 class Knight(threading.Thread):
     def __init__(self, name, power):
         super().__init__()
-        self.name = str(name)
-        self.power = int(power)
+        self.name = name
+        self.power = power
         self.enemies = 100
+
     def run(self):
         print(f"{self.name}, на нас напали!")
         days = 0
@@ -14,12 +15,16 @@ class Knight(threading.Thread):
             self.enemies -= self.power
             days += 1
             time.sleep(1)
-            print(f'{self.name} сражается {days} день(дня), осталось {self.enemies} воинов.')
+            with lock:
+                print(f"{self.name} сражается {days} дней(дня), осталось {self.enemies} воинов.")
 
-        print(f'{self.name} одержал победу спустя {days} дней!')
+        with lock:
+            print(f"{self.name} одержал победу спустя {days} дней(дня)!")
 
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
+
+lock = threading.Lock()
 
 first_knight.start()
 second_knight.start()
